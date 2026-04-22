7aa67cda83275d1394cb281d9404329f1f8dc493
Kone Tshivhinda <erictshivhinda@gmail.com>
2026-04-22 20:26:32 +0200

    Copyright header added to all relevant files

---
+++ src/test/unit/matter-lookup.test.js
@@ -0,0 +1,9 @@
+/**
+ * TimeLex — Automated Legal Time Capture
+ * Copyright (c) [2026] [Kone Tshivhinda]
+ * 
+ * For assessment purposes only - not for production use
+ * 
+ * @license [MIT or Assessment-Specific]
+ * @see LICENSE file in repository root
+ */
